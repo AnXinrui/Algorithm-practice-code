7f43c20635129f314a92abc361da27497a438d44
antian <your_email@2949368301@qq.com>
2023-11-16 11:28:23 +0800

    1115

--- luogu/p4551.cpp
+++ luogu/p4551.cpp
@@ -9,30 +9,48 @@ const int inf = 0x3f3f3f3f;
 const int mod = 1e9 + 7;
 const int N = 1e5 + 10;
 
-int h[N], e[N], ne[N], val[N], idx, d[N];
-
-int n, ans;
-
-void add(int a, int b) {
+int h[N], e[N * 2], ne[N * 2], w[N * 2], idx;
+int son[N * 30][2], cnt, v[N];
+int n;
+void add(int a, int b, int c) {
     e[idx] = b;
+    w[idx] = c;
     ne[idx] = h[a];
     h[a] = idx ++;
 }
 
-void dfs(int he, int s) {
-    // cout << he <<' '<<s<<' '<<h[he]<<endl;
-    int i = h[he];
-    if (i == -1) {
-        ans = max(ans, s);
-        return;
-    }
-
-    for (; i != -1; i = ne[i]) {
+void dfs(int p, int u, int sum) {
+    v[u] = sum;
+    for (int i = h[u]; i != -1; i = ne[i])
+    {
         int j = e[i];
-        int v = val[j];
-        dfs(j, s ^ v);
+        if (j == p) continue;
+        dfs(u, j, sum ^ w[i]);
     }
-    // cout << head << ' ' << res << endl;
+}
+
+void insert(int x) {
+    int p = 0;
+    for (int i = 30; i >= 0; i --) {
+        int u = x >> i & 1;
+        if (!son[p][u])
+            son[p][u] = ++ cnt;
+        p = son[p][u];
+    }
+}
+
+int query(int x) {
+    int p = 0, res = 0;
+    for (int i = 30; i >= 0; i --) {
+        int u = x >> i & 1;
+        if (son[p][!u])
+            res = res * 2 + !u,
+            p = son[p][!u];
+        else
+            res = res * 2 + u,
+            p = son[p][u];
+    }
+    return res;
 }
 
 int main() {
@@ -41,29 +59,23 @@ int main() {
 
     memset(h, -1, sizeof h);
     cin >> n;
-    for (int i = 1; i < n; i ++)
-    {
-        int a, b, w;
-        cin >> a >> b >> w;
-        add(a, b);
-        val[b] = w;
-        ++ d[b];
+    for (int i = 1; i < n; i ++) {
+        int u, v, c;
+        cin >> u >> v >> c;
+        add(u, v, c);
+        add(v, u, c);
     }
-    int head;
+
+    dfs(-1, 1, 0);
+
+    int res = 0;
     for (int i = 1; i <= n; i ++)
-        if (!d[i])
-        {
-            head = i;
-            break;
-        }
-    // for (int i = 0; i <= idx; i ++)
-    //     cout << i << ' '<<h[i]<<' '<<e[i]<<' '<<ne[i]<<endl;
-    for (int i = h[head]; i != -1; i = ne[i])
     {
-        int j = e[i];
-        dfs(j, val[j]);
+        insert(v[i]);
+        res = max(res, v[i] ^ query(v[i]));
     }
-    cout << ans;
+
+    cout << res << "\n";
 
     return 0;
 }
